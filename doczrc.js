bad05a5f1fba6598b883b1a0034f22da6fa08724
yangqianjun <yangqianjun@kuaishou.com>
2019-12-31 19:02:35 +0800

    docs: 修改 base

--- doczrc.js
+++ doczrc.js
@@ -3,11 +3,12 @@
  * @author: yangqianjun
  * @Date: 2019-12-31 17:48:44
  * @LastEditors: yangqianjun
- * @LastEditTime: 2019-12-31 17:56:21
+ * @LastEditTime: 2019-12-31 18:57:21
  */
 
 export default {
   title: 'sm2tsservice',
+  base: 'sm2tsservice',
   typescript: true,
   dest: 'docs',
   filterComponents: files => files.filter(p => p.match(/ts[x]?$/))
