5e61140c444892bbc7dead2027b3f34bb3be1b2d
yangqianjun <yangqianjun@kuaishou.com>
2021-03-30 11:53:46 +0800

    fix: 修复事件绑定 bug

--- static/service.ts
+++ static/service.ts
@@ -5,13 +5,23 @@
 
 // @ts-ignore service.ts 编译规则和 src 不一致
 // eslint-disable-next-line prettier/prettier
-declare let  jsondiffpatch: any;
+declare let jsondiffpatch: any;
 /** 差异 */
 declare let delta: any;
 /** 当前 swagger 版本 */
 declare let curVersion: any;
 /** 新 swagger 版本 */
 declare let diffVersion: any;
+
+/** 新 swagger 版本 */
+declare let newVersion: any;
+
+declare const Autos: {
+  exports: {
+    getExplicitModelDeps(obj: {} | undefined): string[] | undefined;
+    resolveModelDeps(models?: string[], definitions?: any, resolvedDeps?: any): void;
+  };
+};
 
 {
   /* eslint-disable no-undef */
@@ -68,7 +78,7 @@ declare let diffVersion: any;
   /** 仅不选择删除 */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function excludeDelete(me: HTMLInputElement) {
-    setTimeout(function() {
+    setTimeout(function () {
       const all = document.getElementById('全选/取消全选') as HTMLInputElement;
       all.checked = true;
       toggleSelectAll(all);
@@ -78,7 +88,7 @@ declare let diffVersion: any;
   /** 仅选择新增 */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function onlyAdd(me: HTMLInputElement) {
-    setTimeout(function() {
+    setTimeout(function () {
       const all = document.getElementById('全选/取消全选') as HTMLInputElement;
       all.checked = true;
       toggleSelectAll(all);
@@ -109,13 +119,13 @@ declare let diffVersion: any;
       modified
         ? 'show-modified-only canvas'
         : addedAndDeleted
-        ? 'show-added-deleted-only canvas'
-        : added
-        ? 'show-added-only canvas'
-        : deleted
-        ? 'show-add-deleted canvas'
-        : 'canvas'
-    }${propertyNameOnly ? ' show-only-property-name' : ''}`;
+          ? 'show-added-deleted-only canvas'
+          : added
+            ? 'show-added-only canvas'
+            : deleted
+              ? 'show-add-deleted canvas'
+              : 'canvas'
+      }${propertyNameOnly ? ' show-only-property-name' : ''}`;
   }
   const help = `<div class="help-info" id="help-info" style="display: none;">
   <b>查看更新信息</b>
@@ -129,6 +139,8 @@ declare let diffVersion: any;
   <p>
   通过“选择*”相关的选择框，选择性同步远端Swagger文档差异到本地，比如在后端多分支并行开发的情况下，会出现“误删除”差异，这个时候可以通过“仅不选择删除”来同步除了删除之外的所有差异。
   </p>
+  <b>自动选择依赖</b>
+  <p>按住 shift 按键勾选新增的接口 path 或者 model，将自动解析并选中其依赖的其他 Model。</p>
   <b>社区支持</b>
   <p>wx+: skipper_yqj</p>
   </div>`;
@@ -190,14 +202,14 @@ declare let diffVersion: any;
     `
         : `
     <input id="${name}" onClick="${callback}(this)" type="checkbox" name="${name}"><label id="for${name}" for="${name}">${name}${
-            extra !== undefined ? extra : ''
-          }</label>
+        extra !== undefined ? extra : ''
+        }</label>
     `;
       return html;
     }, '') + `<a class="help-icon" onClick="toggleHelp(event)">?</a>${help}`;
   const counter = document.createElement('div');
   counter.className = 'select-count';
-  menu.appendChild(counter);
+  document.body.appendChild(counter);
 
   function setCount() {
     counter.innerHTML = `已选：${getAllSelect().length} / ${getAllChanged().length} 处`;
@@ -256,18 +268,54 @@ declare let diffVersion: any;
     );
   }
 
+  function onAddChecked(value: string) {
+    const [type, key] = value.split(keySeparator);
+    const definitions = newVersion[type][key];
+    const deps = Autos.exports.getExplicitModelDeps(definitions);
+    if (deps) {
+      const resolvedDeps: any = {};
+      Autos.exports.resolveModelDeps(deps, definitions, resolvedDeps);
+      Object.keys(resolvedDeps).forEach(model => {
+        const input: HTMLInputElement | null = document.querySelector(
+          `input[value='definitions${keySeparator}${model}']`
+        );
+        if (input) {
+          input.checked = true;
+          setCount();
+        }
+      });
+    }
+  }
+
   const allChanged = getAllChanged();
   [].forEach.call(allChanged, (node: HTMLElement) => {
     const input = document.createElement('input');
     input.className = ChangedClassName;
     input.type = 'checkbox';
     input.value = getKeys(node).join(keySeparator);
-    input.onchange = () => {
+    input.onclick = (e: MouseEvent) => {
+      if (
+        e.shiftKey &&
+        (e.target as HTMLInputElement).checked &&
+        ((e.target as HTMLInputElement).parentNode as HTMLLIElement).className.indexOf(
+          'jsondiffpatch-added'
+        ) !== -1
+      ) {
+        onAddChecked((e.target as HTMLInputElement).value);
+      }
       setCount();
     };
     node.insertBefore(input, node.firstChild);
-    const handleClick = () => {
+    const handleClick = (e: Event) => {
       input.checked = !input.checked;
+      if (input.checked) {
+        if (
+          (e as MouseEvent).shiftKey &&
+          (input.parentNode as HTMLLIElement).className.indexOf('jsondiffpatch-added') !== -1
+        ) {
+          onAddChecked(input.value);
+        }
+      }
       setCount();
     };
     (node.querySelector('.jsondiffpatch-value') as HTMLButtonElement).onclick = handleClick;
@@ -295,7 +343,7 @@ declare let diffVersion: any;
       JSON.stringify({ version: diffVersion, keys: allSelectedIndexes, unkeys: allExcludedIndexes })
     );
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
         try {
           const res = JSON.parse(xhr.response);
@@ -313,13 +361,13 @@ declare let diffVersion: any;
         }
       }
     };
-    xhr.onload = function() {
+    xhr.onload = function () {
       if (xhr.status !== 200) {
         alert('同步失败');
       }
     };
 
-    xhr.onerror = function(e) {
+    xhr.onerror = function (e) {
       alert('同步失败：' + e && e['message']);
     };
   }
