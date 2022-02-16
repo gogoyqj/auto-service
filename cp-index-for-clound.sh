48c683208afef54522748b41a8cc7aa01d21bec8
yangqianjun <yangqianjun@kuaishou.com>
2022-02-16 18:30:28 +0800

    docs: for cloud

---
+++ cp-index-for-clound.sh
@@ -0,0 +1,14 @@
+if [ -d docs ]; then
+  files=$(find src -name "*.md*")" "$(ls *.md*)
+
+  for file in $files; do
+    if [ -f $file ]; then
+      content=$(grep -o -e "route: [^ ]\+" $file)
+      tar=$(node -e "console.log('${content}'.replace(/route: /g, ''))")
+      if [ "${tar/\//}" != "" ]; then
+        mkdir -p docs/$tar
+        cp -f docs/index.html docs/$tar/index.html
+      fi
+    fi
+  done
+fi
