520d7125a9fa662bd679a14734b4067fec9c7c07
skipper <ivugogo@gmail.com>
2023-07-25 12:46:43 +0800

    tests: add tests for fix

--- __tests__/yapi/yapi2swagger.test.ts
+++ __tests__/yapi/yapi2swagger.test.ts
@@ -4,9 +4,10 @@ import yapiJSON2swagger, {
   convertParams,
   convertResponse
 } from 'src/yapi/yapi2swagger';
-import { data, yapiConfig } from '../yapidata';
+import { mockData, yapiConfig, yapiConfigCompatible } from '../yapidata';
 
-describe('yapi2swagger should work ok', () => {
+describe('yapi2swagger custom should work ok', () => {
+  const data = mockData();
   const syntheticAPI = {
     ...data[0].list[1],
     url: '/test'
@@ -36,5 +37,39 @@ describe('yapi2swagger should work ok', () => {
 
   it('yapiJSON2swagger should work', () => {
     expect(yapiJSON2swagger(data, yapiConfig)).toMatchSnapshot('yapiJSON2swagger yapiJSON2swagger');
+  });
+});
+
+describe('yapi2swagger should work ok', () => {
+  const data = mockData();
+  const syntheticAPI = {
+    ...data[0].list[1],
+    url: '/test'
+  };
+
+  it('obtainMeta should work', () => {
+    expect(obtainMeta(data, yapiConfigCompatible)).toMatchSnapshot('yapiJSON2swagger obtainMeta');
+  });
+
+  it('convertCosumes should work', () => {
+    expect(convertCosumes(syntheticAPI, yapiConfigCompatible)).toMatchSnapshot(
+      'yapiJSON2swagger convertCosumes'
+    );
+  });
+
+  it('convertParams should work', () => {
+    expect(convertParams(syntheticAPI, yapiConfigCompatible)).toMatchSnapshot(
+      'yapiJSON2swagger convertParams'
+    );
+  });
+
+  it('convertResponse should work', () => {
+    expect(convertResponse(syntheticAPI, yapiConfigCompatible)).toMatchSnapshot(
+      'yapiJSON2swagger convertResponse'
+    );
+  });
+
+  it('yapiJSON2swagger should work', () => {
+    expect(yapiJSON2swagger(data, yapiConfigCompatible)).toMatchSnapshot('yapiJSON2swagger yapiJSON2swagger');
   });
 });
